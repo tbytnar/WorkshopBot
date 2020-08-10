cae72d9bb66833435ac599c18072cf5176df485a
Tim Bytnar <tbytnar@gmail.com>
2020-08-10 13:05:55 -0500

    Added timer and set to every 15 minutes..

--- bot.js
+++ bot.js
@@ -1,8 +1,15 @@
 const { Client, MessageEmbed } = require('discord.js');
 const fetch = require('node-fetch');
+const debug = true;
 
 const client = new Client();
 const prefix = '!';
+
+var NOTIFY_CHANNEL;
+client.on('ready', () => {
+	NOTIFY_CHANNEL = client.channels.cache.get('688880181410398227'); // Channel to send notification
+	console.log('Ready!');
+});
 
 var collectionId = "2178843757";
 var token = process.env.DISCORD_TOKEN;
@@ -31,11 +38,55 @@ catch (error)
 	console.log(error);
 }
 
+function checkCollection(collectionId)
+{
+	var totalItems = Object.keys(collection).length;
+	var totalItemsUpdated = 0;
 
-client.once('ready', () => {
-	console.log('Ready!');
-});
+	for (var i = 0; i < totalItems; i++)
+	{
+		var itemBody = [];
+		var itemId = collection[i].publishedfileid;
+		itemBody.push("itemcount=1&publishedfileids[0]=" + itemId);
 
+		try {
+			(async () => {
+				const response = await fetch('https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/', 
+												{ method: 'POST',
+												headers: {
+													'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+												},
+												body: itemBody }
+											); 
+				const json = await response.json();
+
+				var itemLastUpdate = new Date(json.response.publishedfiledetails[0].time_updated * 1000);
+				var itemURL = 'https://steamcommunity.com/sharedfiles/filedetails/?id=' + itemId;
+				
+				const itemEmbed = new MessageEmbed()
+				.setColor('#0099ff')
+				.setTitle(json.response.publishedfiledetails[0].title)
+				.setURL(itemURL)
+				.setThumbnail(json.response.publishedfiledetails[0].preview_url)
+				.addField('Last Updated', itemLastUpdate.toString(), true)
+
+				if (itemLastUpdate > new Date(Date.now() - 15 * 60 * 1000))
+				{
+					totalItemsUpdated ++;
+					message.channel.send(itemEmbed);
+				}
+			})();	
+		} 
+		catch (error)
+		{
+			console.log(error);
+		}	
+	}
+
+	if(totalItemsUpdated == 0 && debug) { NOTIFY_CHANNEL.send('No mods have been updated recently!'); }
+}
+
+setInterval(function() { checkCollection(collectionId); }, 15 * 60 * 1000); // Check every 15 minutes
 
 client.on('message', async message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
@@ -44,63 +95,7 @@ client.on('message', async message => {
 	const command = args.shift().toLowerCase();
 
 	if (command === 'checkmods') {
-		console.log(collection);
-
-		var totalItems = Object.keys(collection).length;
-
-		for (var i = 0; i < totalItems; i++)
-		{
-			var itemBody = [];
-			var itemId = collection[i].publishedfileid;
-			itemBody.push("itemcount=1&publishedfileids[0]=" + itemId);
-
-			try {
-				(async () => {
-					const response = await fetch('https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/', 
-													{ method: 'POST',
-													headers: {
-														'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-													},
-													body: itemBody }
-												); 
-					const json = await response.json();
-
-					var itemTitle = json.response.publishedfiledetails[0].title; 
-					var itemLastUpdate = new Date(json.response.publishedfiledetails[0].time_updated * 1000);
-					var itemURL = 'https://steamcommunity.com/sharedfiles/filedetails/?id=' + itemId;
-					var itemMessage = itemTitle + "\n" + itemLastUpdate.toString();
-					
-					const exampleEmbed = new MessageEmbed()
-					.setColor('#0099ff')
-					.setTitle(json.response.publishedfiledetails[0].title)
-					.setURL(itemURL)
-					//.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-					//.setDescription('Some description here')
-					.setThumbnail(json.response.publishedfiledetails[0].preview_url)
-					//.addFields(
-					//	{ name: 'Regular field title', value: 'Some value here' },
-					//	{ name: '\u200B', value: '\u200B' },
-					//	{ name: 'Inline field title', value: 'Some value here', inline: true },
-					//	{ name: 'Inline field title', value: 'Some value here', inline: true },
-					//)
-					.addField('Last Updated', itemLastUpdate.toString(), true)
-					//.setImage('https://i.imgur.com/wSTFkRM.png')
-					//.setTimestamp(itemLastUpdate)
-					//.setFooter('Last Updated', itemLastUpdate.toString());
-
-
-					if (itemLastUpdate > new Date(Date.now() - 1 * 24 * 60 * 60 * 1000))
-					{
-						message.channel.send(exampleEmbed);
-					}
-					
-				})();	
-			} 
-			catch (error)
-			{
-				console.log(error);
-			}	
-		}
+		checkCollection(collectionId);
 	}
 });
 
