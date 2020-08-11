4941f738c87f1c39323d0cc77334a8368c0277ad
Tim Bytnar <tbytnar@gmail.com>
2020-08-11 09:22:24 -0500

    QOL changes

--- bot.js
+++ bot.js
@@ -1,5 +1,3 @@
-const debug = true;
-
 const { Client, MessageEmbed } = require('discord.js');
 const fetch = require('node-fetch');
 const Sequelize = require('sequelize');
@@ -27,31 +25,31 @@ const Guilds = sequelize.define('guilds', {
 	debug: Sequelize.BOOLEAN,
 });
 
-async function getCollection(collectionId) {
-	try {
-		const collectionBody = [];
-		collectionBody.push('collectioncount=1&publishedfileids[0]=' + collectionId);
+async function sleep(millis) {
+	console.log('Sleeping for ' + millis + ' ms');
+	return new Promise(resolve => setTimeout(resolve, millis));
+}
 
-		const response = await fetch('https://api.steampowered.com/ISteamRemoteStorage/GetCollectionDetails/v1/',
-			{ method: 'POST',
-				headers: {
-					'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-				},
-				body: collectionBody },
-		);
-		const json = await response.json();
-		return json.response.collectiondetails[0].children;
-	}
-	catch (error) {
-		console.log(error);
-	}
+async function getCollection(collectionId) {
+	const collectionBody = [];
+	collectionBody.push('collectioncount=1&publishedfileids[0]=' + collectionId);
+
+	const response = await fetch('https://api.steampowered.com/ISteamRemoteStorage/GetCollectionDetails/v1/',
+		{ method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+			},
+			body: collectionBody },
+	);
+	return response.json();
 }
 
 async function notifyOnline() {
 	const allGuilds = await Guilds.findAll();
 	for (const guild of allGuilds) {
 		const notify_channel = client.channels.cache.get(guild.notify_channel_id);
-		notify_channel.send('Workshopt bot is now online!');
+		notify_channel.send('Workshop bot is now online!');
+		// checkAll();
 	}
 }
 
@@ -60,60 +58,56 @@ async function checkAll() {
 	console.log('Checking collections for ' + allGuilds.length + ' servers.');
 
 	for (const guild of allGuilds) {
+		await sleep(250);
 		const notify_channel = client.channels.cache.get(guild.notify_channel_id);
 		const collectionId = guild.collection_id;
-		try {
-			await checkCollection(collectionId, notify_channel);
-		}
-		catch (error)	{
-			console.log(error);
-		}
+		await checkCollection(collectionId, notify_channel);
 	}
 }
 
-const checkCollection = async (collectionId, notify_channel) => {
-	const collectionBody = [];
-	collectionBody.push('collectioncount=1&publishedfileids[0]=' + collectionId);
-	getCollection(collectionId).then(function(collection) {
-		for (const item of collection) {
-			const itemBody = [];
-			const itemId = item.publishedfileid;
-			itemBody.push('itemcount=1&publishedfileids[0]=' + itemId);
-			try {
-				(async () => {
-					const response = await fetch('https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/',
-						{ method: 'POST',
-							headers: {
-								'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' },
-							body: itemBody });
-					const json = await response.json();
+async function checkCollection(collectionId, notify_channel) {
+	const collection = await getCollection(collectionId);
+	const collectionItems = collection.response.collectiondetails[0].children;
+	let totalItemsUpdated = 0;
+	for (const item of collectionItems) {
+		await sleep(250);
+		const itemBody = [];
+		const itemId = item.publishedfileid;
+		itemBody.push('itemcount=1&publishedfileids[0]=' + itemId);
+		try {
+			(async () => {
+				const response = await fetch('https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/',
+					{ method: 'POST',
+						headers: {
+							'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' },
+						body: itemBody });
+				const json = await response.json();
 
-					console.log('Checking: ' + json.response.publishedfiledetails[0].title);
-					const itemLastUpdate = new Date(json.response.publishedfiledetails[0].time_updated * 1000);
-					const itemURL = 'https://steamcommunity.com/sharedfiles/filedetails/?id=' + itemId;
+				console.log('Checking: ' + json.response.publishedfiledetails[0].title);
+				const itemLastUpdate = new Date(json.response.publishedfiledetails[0].time_updated * 1000);
+				const itemURL = 'https://steamcommunity.com/sharedfiles/filedetails/?id=' + itemId;
 
-					const itemEmbed = new MessageEmbed()
-						.setColor('#0099ff')
-						.setTitle(json.response.publishedfiledetails[0].title)
-						.setURL(itemURL)
-						.setThumbnail(json.response.publishedfiledetails[0].preview_url)
-						.addField('Last Updated', itemLastUpdate.toString(), true);
+				const itemEmbed = new MessageEmbed()
+					.setColor('#0099ff')
+					.setTitle(json.response.publishedfiledetails[0].title)
+					.setURL(itemURL)
+					.setThumbnail(json.response.publishedfiledetails[0].preview_url)
+					.addField('Last Updated', itemLastUpdate.toString(), true);
 
-					const fourDays = new Date(Date.now() - 4 * 24 * 60 * 60 * 1000);
-					const fifteenMinutes = new Date(Date.now() - 15 * 60 * 1000);
-					if (itemLastUpdate > fourDays) {
-						if(itemEmbed !== null) { notify_channel.send(itemEmbed); }
-					}
-				})();
-			}
-			catch (error) {
-				console.log(error);
-			}
-			// 4 requests per second
-			setTimeout(() => console.log('Pausing api calls.'), 250);
+				// const fourDays = new Date(Date.now() - 4 * 24 * 60 * 60 * 1000);
+				const fifteenMinutes = new Date(Date.now() - 15 * 60 * 1000);
+				if (itemLastUpdate > fifteenMinutes) {
+					if(itemEmbed !== null) { notify_channel.send(itemEmbed); }
+					totalItemsUpdated++;
+				}
+			})();
 		}
-	});
-};
+		catch (error) {
+			console.log(error);
+		}
+	}
+	if(totalItemsUpdated === 0) { notify_channel.send('No items updated recently'); }
+}
 
 client.on('ready', () => {
 	Guilds.sync();
@@ -147,6 +141,7 @@ client.on('message', async message => {
 		}
 		else { message.channel.send('Could not locate a configuration for your server.'); }
 	}
+	else if (command === 'wsbstatus') { message.channel.send('Im still here!'); }
 	else if (command === 'wsbconfig') {
 		try {
 			// equivalent to: INSERT INTO tags (name, description, username) values (?, ?, ?);
